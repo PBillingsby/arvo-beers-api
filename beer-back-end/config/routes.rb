3cedfd981cf52d442734fca3d4cd4c729960b3e8
PBillingsby <philbillingsby@gmail.com>
2020-09-03 14:08:53 -0700

    Remove old directory

--- beer-back-end/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # resources :beers, only: [:create, :show, :index]
-  scope '/api/v1' do
-    resources :beers, only: [:create, :show, :index]
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
