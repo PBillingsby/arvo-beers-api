19e5978952f4e005adc49938254c9b7d62bc8065
PBillingsby <philbillingsby@gmail.com>
2020-09-03 18:12:27 -0700

    Add distinct countries to state

--- ab-front-end/src/container/Beers.js
+++ ab-front-end/src/container/Beers.js
@@ -4,35 +4,51 @@ import BeerForm from "../BeerForm";
 import Beer from "../Beer";
 
 export default class Beers extends Component {
-  state = {
-    beers: []
-  };
+  constructor() {
+    super();
+    this.state = {
+      beers: [],
+      distinctCountries: []
+    };
+  }
+
   componentDidMount() {
     this.getBeers();
   }
+
   listByCountry() {
     debugger;
   }
+
   getBeers = () => {
     fetch("http://localhost:3001/api/v1/beers")
       .then(resp => resp.json())
       .then(beer => {
-        this.setState({ beers: beer });
+        this.setState({
+          beers: beer,
+          distinctCountries: []
+        });
       });
   };
+
+  componentDidUpdate() {
+    if (this.state.beers.length > 0) {
+      return [...new Set(this.state.beers.map(beer => beer.country))].map(
+        beer => (
+          <option key={beer.country + beer.id} value={beer.country}>
+            {beer.country}
+          </option>
+        )
+      );
+    }
+  }
 
   render() {
     return (
       <>
         <div className="row p-3">
           <p>By country: </p>
-          <select onChange={this.listByCountry}>
-            {this.state.beers.map(beer => (
-              <option key={beer.country + beer.id} value={beer.country}>
-                {beer.country}
-              </option>
-            ))}
-          </select>
+          <select onChange={this.listByCountry}>{this.getCountries()}</select>
         </div>
         <div className="row text-center">
           {this.state.beers.map(beer => (
