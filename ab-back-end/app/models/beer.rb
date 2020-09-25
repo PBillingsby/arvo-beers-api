c1547b366ed2e55d33c430c44d2b5ee482f94020
PBillingsby <philbillingsby@gmail.com>
2020-09-25 08:21:48 -0700

    Update gemfile and remove seeds

--- ab-back-end/app/models/beer.rb
+++ ab-back-end/app/models/beer.rb
@@ -1,7 +1,16 @@
+require 'httparty'
 
 class Beer < ApplicationRecord
+  include HTTParty
   include Rails.application.routes.url_helpers
   has_one_attached :avatar
+  before_create :scrape_information
+
+
+  def scrape_information
+    unparsed_url = HTTParty.get('https://www.ratebeer.com/search?q=victoria+bitter&tab=beer')
+    doc = Nokogiri::HTML(unparsed_url)
+  end
 
   def avatar_url
     [url_for(self.avatar), self.id] if self.avatar.attached?
